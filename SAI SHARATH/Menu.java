ce63a1959ba1fd90dcf3b41e3b260b42f75157f4
MAALE SAI SHARATH <malle.saisharath@gmail.com>
2017-09-27 23:44:36 +0530

    Update Menu.java

--- SAI SHARATH/Menu.java
+++ SAI SHARATH/Menu.java
@@ -1,49 +1,93 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 
 public class Menu {
 	
 		
-			Item item=new Item();
+			
 			//main course.........
 			List<Item> mainCourse=new ArrayList<Item>();
 			
-			item.setItemId(1);
-			item.setItemName("biryani");
-			item.setItemCost(250);
-			mainCourse.add(item);
-			
-			item.setItemId(2);
-			item.setItemName("fried rice");
-			item.setItemCost(250);
-			mainCourse.add(item);
-		
 			//deserts.........
 			List<Item> deserts=new ArrayList<Item>();
 			
-			item.setItemId(1);
-			item.setItemName("choco Ice cream");
-			item.setItemCost(20);
-			deserts.add(item);
-			
-			item.setItemId(2);
-			item.setItemName("venilla Ice cream");
-			item.setItemCost(20);
-			deserts.add(item);
-			
 			//starters..
 			List<Item> starters=new ArrayList<Item>();
+
+			public void getMenu()
+			{
+				Item item=new Item();
+				
+				item.setItemId(1);
+				item.setItemName("biryani");
+				item.setItemCost(250);
+				mainCourse.add(item);
+				
+				item.setItemId(2);
+				item.setItemName("fried rice");
+				item.setItemCost(250);
+				mainCourse.add(item);
+				
+				item.setItemId(1);
+				item.setItemName("choco Ice cream");
+				item.setItemCost(20);
+				deserts.add(item);
+				
+				item.setItemId(2);
+				item.setItemName("venilla Ice cream");
+				item.setItemCost(20);
+				deserts.add(item);
+				
+				item.setItemId(1);
+				item.setItemName("tanduri");
+				item.setItemCost(260);
+				starters.add(item);
+				
+				item.setItemId(2);
+				item.setItemName("chicken lollipop");
+				item.setItemCost(260);
+				starters.add(item);
+				
+				getSingleMenu(mainCourse,"Main Course");
+				getSingleMenu(deserts,"Deserts");
+				getSingleMenu(starters,"Starters");
+				
+				
+				
+			}
 			
-			item.setItemId(1);
-			item.setItemName("tanduri");
-			item.setItemCost(260);
-			starters.add(item);
+			public void getSingleMenu(List<Item> items,String type)
+			{
 			
-			item.setItemId(1);
-			item.setItemName("chicken lollipop");
-			item.setItemCost(260);
-			starters.add(item);
-	
+				Iterator itr=items.iterator();
+				System.out.println(type);	
+				System.out.println("ItemId:"+"\t"+"Item Name"+"\t"+"Item Cost");
+			
+				while(itr.hasNext())
+				{
+			
+					Item item=(Item)itr.next();								
+					System.out.println(item.getItemId()+"\t"+item.getItemName()+	"\t"+item.getItemCost());
+			
+				}
+
+			}
+			
+			public List<Item> getMainCourse() {
+				return mainCourse;
+			}
+
+			public List<Item> getDeserts() {
+				return deserts;
+			}
+
+			public List<Item> getStarters() {
+				return starters;
+			}
+			
+			
+			
 
 }
