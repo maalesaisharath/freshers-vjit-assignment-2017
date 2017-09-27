deca0373a25822464e1d8f833f932acbbe54893b
MAALE SAI SHARATH <malle.saisharath@gmail.com>
2017-09-27 23:42:40 +0530

    Update Manager.java

--- SAI SHARATH/Manager.java
+++ SAI SHARATH/Manager.java
@@ -1,71 +1,32 @@
+import java.util.List;
+
 
 public class Manager {
 	
-			List<Item> item=new ArrayList<Item>();
-			Item.add("desert",1,"choco Ice cream",20);
-			Item.add("desert",2,"venilla Ice cream",20);
-			Item.add("starter",3,"tanduri",250);
-			Item.add("starter",4,"chicken lollipop",230);
-			Item.add("main course",5,"biryani",250);
+	Menu menu=new Menu();
 		
 			public void getMenu()
 			{
-			Iterator itr=items.iterator();
-			System.out.println("Deserts");	System.out.println("ItemId:"+"\t"+"Item Name"+"\t"+"Item Cost");
-		
-			while(itr.hasNext())
-			{
-		
-				Item item=(Order)itr.next();
-				if(item.getItemType().equals("desert"))
-				{									System.out.println(item.getItemId()+"\t"+item.getItemName()+	"\t"+item.getItemCost());
-				}
-		
+				menu.getMenu();
 			}
-			System.out.println("Starter");	System.out.println("ItemId:"+"\t"+"Item Name"+"\t"+"Item Cost");
-		
-			while(itr.hasNext())
+			public void placeOrder(Order order)
 			{
-		
-				Item item=(Order)itr.next();
-				if(item.getItemType().equals("starter"))
-				{									System.out.println(item.getItemId()+"\t"+item.getItemName()+	"\t"+item.getItemCost());
-				}
-		
+				ExecutiveChef executiveChef=new ExecutiveChef();
+				executiveChef.placeOrder(order);
 			}
-			System.out.println("Main Course");	System.out.println("ItemId:"+"\t"+"Item Name"+"\t"+"Item Cost");
-		
-			while(itr.hasNext())
+			public List<Item> getMainCourseMenu()
 			{
-		
-				Item item=(Order)itr.next();
-				if(item.getItemType().equals("main course"))
-				{									System.out.println(item.getItemId()+"\t"+item.getItemName()+	"\t"+item.getItemCost());
-				}
-		
+				return menu.getMainCourse();
 			}
-			}
-			public Item getItem(int orderId)
+			public List<Item> getStartereMenu()
 			{
-				Iterator itr=items.iterator();	
-				Item ordereditem=null; 
-		
-				while(itr.hasNext())
-				{
-					Item item=(Order)itr.next();
-					if(item.getItemId==orderId)
-					{
-						ordereditem=item;
-						break;		
-					}
-				}
-				return ordereditem;
-			}	
-			public List<Item> placeOrder(Order order)
-			{
-				
-				
-				return order.getOrder();
+				return menu.getStarters();
 			}
+			public List<Item> getDesertMenu()
+			{
+				return menu.getDeserts();
+			}
+			
+			
 
 }
