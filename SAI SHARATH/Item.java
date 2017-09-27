5119b983869b4a00ec10fe0256ce0296552914d3
MAALE SAI SHARATH <malle.saisharath@gmail.com>
2017-09-27 17:01:44 +0530

    Add files via upload

---
+++ SAI SHARATH/Item.java
@@ -0,0 +1,27 @@
+
+public class Item {
+		private int itemId;
+		private String itemName;
+		private double itemCost;
+		
+		public int getItemId() {
+			return itemId;
+		}
+		public void setItemId(int itemId) {
+			this.itemId = itemId;
+		}
+		public String getItemName() {
+			return itemName;
+		}
+		public void setItemName(String itemName) {
+			this.itemName = itemName;
+		}
+		public double getItemCost() {
+			return itemCost;
+		}
+		public void setItemCost(double itemCost) {
+			this.itemCost = itemCost;
+		}
+		
+	
+}
