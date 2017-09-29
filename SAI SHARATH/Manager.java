dc3ddc26689f16fa395c69f4177e2ff2b90b7b02
MAALE SAI SHARATH <malle.saisharath@gmail.com>
2017-09-29 16:33:54 +0530

    Update Manager.java

--- SAI SHARATH/Manager.java
+++ SAI SHARATH/Manager.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Manager {
 	
 	Menu menu=new Menu();
-		
+	
 			public void getMenu()
 			{
 				menu.getMenu();
@@ -14,19 +14,20 @@ public class Manager {
 				ExecutiveChef executiveChef=new ExecutiveChef();
 				executiveChef.placeOrder(order);
 			}
-			public List<Item> getMainCourseMenu()
+			public void getBill(Order order)
 			{
-				return menu.getMainCourse();
+				double totalCost=0;
+				Bill bill=new Bill(order,menu.getMainCourse(),menu.getDeserts(),menu.getStarters());
+				List<Item> itemsBill=bill.getBill();
+				System.out.println("*******************Bill*****************");
+				System.out.println("ItemId:"+"\t"+"Item Name"+"\t"+"Quatity"+"\t"+"Cost");
+				for(Item item : itemsBill)
+				{
+					System.out.println(item.getItemId()+"\t"+item.getItemName()+"\t"+item.getQuantity()+"\t"+item.getItemCost());
+					totalCost=totalCost+item.getItemCost();
+				}
+				System.out.println("Total Cost:\t\t\t\t"+totalCost);
+				
 			}
-			public List<Item> getStartereMenu()
-			{
-				return menu.getStarters();
-			}
-			public List<Item> getDesertMenu()
-			{
-				return menu.getDeserts();
-			}
-			
-			
 
 }
